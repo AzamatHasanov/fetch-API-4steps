fa63fe56fec418dbc13a04062ef1124dafa406a3
Azamat Hasanov <politicalhe03@gmail.com>
2024-09-10 16:17:54 -0700

    change Api v3

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// step 1 FETCH Get Method
+
 document.getElementById("fetchData").addEventListener("click", () => {
     fetch("https://jsonplaceholder.typicode.com/posts/1")
         // Bu URL jsonplaceholder API-dən bir post məlumatını çəkir.
@@ -12,3 +14,42 @@ document.getElementById("fetchData").addEventListener("click", () => {
             console.error("Error:", error);
         });
 });
+
+// step 2 Post Methodu
+
+document.getElementById('sendData').addEventListener('click', () => {
+    const data = {
+        name: document.getElementById('inputData').value
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById('data').innerHTML =
+                `<h1>ResPonse: </h1>
+                <pre>${JSON.stringify(data,null,2)}</pre>`
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        })
+
+});
+
+
+
+
+
+// document.getElementById('fetchData').addEventListener('click', () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts/1')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Received data:', data); // JSON cavabını konsolda görmək
+//             // `data` obyektinin içərisində `title` və `body` xüsusiyyətlərini yoxlayın.
+//         })
+//         .catch(error => console.error('Error:', error));
+// });
